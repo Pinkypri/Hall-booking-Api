5b5209cb1d67d9f341db5db8ecbe57c312e7da09
unknown <priya.agri1999@gmail.com>
2021-07-03 00:39:51 -0700

    update routes

--- routes/api/user/index.js
+++ routes/api/user/index.js
@@ -24,108 +24,120 @@ router.post("/room",async(req,res)=>{
 });
 
 
-
- router.post("/book",async(req,res)=>{
-       
-        try {
-             const room=new Room(req.body);
-            const checkRoom=await Room.findById({ _id:req.body.bookingDetails});
-            if(checkRoom.hall_booking ==""){
-              let startDate=new Date(`${req.body.startDate}`);
-              let endDate=new Date(`${req.body.endDate}`);
-              let start=new Date(`${req.body.startDate}`+`${req.body.startTime}`);
-              let end=new Date(`${req.body.endDate}`+`${req.body.endTime}`);
-              let now=new Date();
-              if(start<now){
-                  return res.json({msg:"Date is expired"});
-              }
-              if(end <= start){
-                  return res.json({msg:"Date not found"});
-              }
-              if(startDate=="Invalid Date" || endDate=="Invalid Date"){
-                  return res.json({msg:"Invalid Date"});
-              }
-              else{
-                  const user=new User(req.body);
-                  const session=await User.startSession();
-                  const userDetails=await user.save({session:session});
-                  const roomDetails=await Room.findByIdAndUpdate(
-                  {_id:userDetails.bookingDetails},
-                  {
-                  $addToSet:{ hall_booking : userDetails._id},
-                  },    
-             {new:true},
-          
-        ).session(session);
-            
-         res.json({user:userDetails,room:roomDetails});
-        }
-    }else{
- 
-        if(checkRoom.hall_booking){
-            const room=await User.find({_id:checkRoom.hall_booking});
-   
-            let startDate=new Date(`${req.body.startDate}`);
-            let endDate=new Date(`${req.body.endDate}`);
-            let start=new Date(`${req.body.startDate}`+`${req.body.startTime}`);
-            let end=new Date(`${req.body.endDate}`+`${req.body.endTime}`);
-            let now=new Date();
-     
-            if(start<now){
-                return res.json({msg:"Date is expired"});
-            }
-            if(end<=start){
-                return res.json({msg:"Date not found"});
-            }
-            if(startDate=="Invalid Date" || endDate=="Invalid Date"){
-                return res.json({msg:"Invalid Date"});
-            }
-            console.log(room);
-          const roomcheck=room.find((room)=>{
-           
-            let d0=new Date(`${room.startDate}`+`${room.startTime}`);
-            let d1=new Date(`${room.endDate}`+`${room.endTime}`);
-    
-              let d2=new Date(`${req.body.startDate}`+`${req.body.startTime}`);
-              let d3=new Date(`${req.body.endDate}`+`${req.body.endTime}`);
-
-              if((d2<d0 && d3<=d0) || (d2<d1 && d3<=d1)){
-                  return true
-              }
-
-          });
+router.post("/book", async (req, res) => {
+    try {
+      const room = new Room(req.body);
+      const checkRoom = await Room.findById({ _id: req.body.bookingDetails });
   
-console.log(!roomcheck);
-        if(roomcheck)  { 
-         
-                return res.json({msg:"room is already booked"});
-             }
-             else{
-            
-          const user=new User(req.body);
-          const session=await User.startSession();
-          const userDetails=await user.save({session:session});
-         const roomDetails=await Room.findByIdAndUpdate(
-        {_id:userDetails.bookingDetails},
-         {
-              $addToSet:{ hall_booking : userDetails._id},
-           },    
-           {new:true},
-        
-      ).session(session);
+      if (checkRoom.hall_booking == "") {
+        var startDate = new Date(`${req.body.startDate}`);
+        var endDate = new Date(`${req.body.endDate}`);
           
-        return res.json({user:userDetails,room:roomDetails});
+  
+        var start = new Date(`${req.body.startDate} ` + `${req.body.startTime}`);
+        var end = new Date(`${req.body.endDate} ` + `${req.body.endTime}`);
+  
+        var now = new Date();
+        
+        if(start<now){
+            return res.json({msg:" date is expired"})
+        }
+  
+        if (end <= start) {
+          return res.json({ msg: "date not found"});
+        }
+  
+        if (startDate == "Invalid Date" || endDate == "Invalid Date") {
+          return res.json({ msg: "invalid date" });
+        } else {
+          let user = new User(req.body);
+          let session = await User.startSession();
+  
+          let userDetails = await user.save({ session: session });
+          let roomDetails = await Room
+            .findByIdAndUpdate(
+              { _id: req.body.bookingDetails },
+              {
+                $addToSet: { hall_booking: user.id },
+              },
+              { new: true }
+            )
+            .session(session);
+  
+          res.json({ user: userDetails, room: roomDetails });
+        }
+      } else {
+         
+        if (checkRoom.hall_booking) {
+          const room = await User.find({ _id: checkRoom.hall_booking });
+      
+  
+          var startDate = new Date(`${req.body.startDate}`);
+          var endDate = new Date(`${req.body.endDate}`);
+  
+          var start = new Date(
+            `${req.body.startDate} ` + `${req.body.startTime}`
+          );
+          var end = new Date(`${req.body.endDate} ` + `${req.body.endTime}`);
+  
+  
+          var now = new Date();
+          
+          if(start<now){
+              return res.json({msg:"date is expired"});
+          }
+  
+          if (end <= start) {
+            return res.json({msg: "date is wrong"});
+          }
+  
+          if (startDate == "Invalid Date" || endDate == "Invalid Date") {
+            return res.json({ msg: "invalid date" });
+          }
+  
+          const roomCheck = room.find((room) => {
+            var start = new Date(`${room.startDate} ` + `${room.startTime}`);
+            var end = new Date(`${room.endDate} ` + `${room.endTime}`);
+  
+            var userstart = new Date(`${req.body.startDate} ` + `${req.body.startTime}`);
+            var userend = new Date(`${req.body.endDate} ` + `${req.body.endTime}`);
+  
+            if ((userstart < start && userend <= start) || (userstart < end && userend <=end)) {
+              return true;
+            }
+          });
+                      
+         
+          if (roomCheck) {
+
+            return res.json({msg:"Sorry,  Room is already booked on this date and time."});
+          } else {
+            let user = new User(req.body);
+            let session = await User.startSession();
+  
+            let userDetails = await user.save({ session: session });
+            let roomDetails = await Room
+              .findByIdAndUpdate(
+                { _id: req.body.bookingDetails },
+                {
+                  $addToSet: { hall_booking: user.id },
+                },
+                { new: true }
+              )
+              .session(session);
+            return res.json({ user: userDetails, room: roomDetails });
+          }
+        }
       }
-  }
+    } catch (error) {
+      res.json({ error });
     }
-            }catch (error) {
-            res.json(error) ;
-         }
-         });
+  });
+  
 
     router.get("/all/customers",async(req,res)=>{
         try {
-            const user=await User.find({}).populate("booking",["name","location","price","amentities"])
+            const user=await User.find({}).populate({path:"bookingDetails",select:"name location seats price amentities -_id"})
             res.json(user);
         } catch (error) {
              res.json(error); 
@@ -135,7 +147,7 @@ console.log(!roomcheck);
 
     router.get("/all/rooms",async(req,res)=>{
         try {
-            const user=await Room.find({}).populate("hall_booking",["customername","date","start","end","status"]);
+            const user=await Room.find({}).populate({path:"hall_booking",select:"customername date start end status -_id"});
             res.json(user);
         } catch (error) {
             res.json(error);
